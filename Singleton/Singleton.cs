da6193c1e0289bf31904abd5362a89b18a1b2409
SelfishCoder <meric.balikci@gmail.com>
2020-06-21 13:36:41 +0300

    Singleton has been added.

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace SelfishCoder.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private static T instance;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (!instance)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (!instance)
+                    {
+                        instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                    }
+
+                    DontDestroyOnLoad(instance);
+                }
+
+                return instance;
+            }
+            private set
+            {
+                if (instance)
+                {
+                    Destroy(value.gameObject);
+                    return;
+                }
+                instance = value;
+                DontDestroyOnLoad(instance);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void Awake()
+        {
+            Instance = this as T;
+        }
+    }
+}
