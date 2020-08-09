2889fa2a5338e85971f30e0faf3260152b0c90a4
Meriç <meric.balikci@gmail.com>
2020-08-09 22:34:12 +0300

    Doc comments and regions have been added.

--- Object Pooling/PoolObject.cs
+++ Object Pooling/PoolObject.cs
@@ -9,27 +9,55 @@ namespace SelfishCoder.Core
     [Serializable]
     public class PoolObject
     {
+        #region Fields
+
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField] private GameObject gameObject;
+        
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField] private bool inUse;
+        
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField] private int id;
 
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
         public GameObject GameObject
         {
             get => gameObject;
             set => gameObject = value;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int Id
         {
             get => id;
             set => id = value;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public bool InUse
         {
             get => inUse;
             set => inUse = value;
         }
+
+        #endregion
     }
     
     /// <summary>
@@ -39,8 +67,23 @@ namespace SelfishCoder.Core
     [Serializable]
     public class PoolObject<T> where T: class
     {
+        #region Fields
+
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField] private T component;
+        
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField] private bool inUse;
+        
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField] private int id;
+
+        #endregion
     }
 }
